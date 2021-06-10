11976213e12da2d662e36d6eff242fb2da1d4fd5
Cameron Stuart <cameron@cameronstuart.com>
2021-06-10 16:35:02 -0400

    adds css

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const path = require('path')
 const app = express()
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/styles', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/css/index.css'));
 });
 
 const port = process.env.PORT || 4005
