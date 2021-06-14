f19fd70c24e9f15edc14ef3ebc3045bfd19e2034
Cameron Stuart <cstuart08@gmail.com>
2021-06-14 11:06:23 -0400

    adds rollbar

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar('d60c83b75da64c969cc075b85aeaa54a')
 const path = require('path')
 
 const app = express()
@@ -10,6 +12,13 @@ app.get('/',function(req,res) {
 app.get('/styles', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/css/index.css'));
 });
+
+app.get('/something', function(req, res) {
+  doSomething()
+  res.sendStatus(400)
+})
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4005
 
